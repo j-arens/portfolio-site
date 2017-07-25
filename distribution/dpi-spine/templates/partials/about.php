2fc12fc74d012dd37948c0f534d3b8f4ae0f2b88
j-arens <arens.joshua@sbcglobal.net>
2017-07-25 11:03:58 -0400

    basic setup of skills

--- distribution/dpi-spine/templates/partials/about.php
+++ distribution/dpi-spine/templates/partials/about.php
@@ -32,5 +32,44 @@
     <div class="skills about__section">
         <p class="about__title">Areas of Skill</p>
         <hr class="about__hr">
+        <ul class="about__list">
+            <li class="about__item">
+                <p class="about__item-meta">Advanced</p>
+                <p class="about__item-title">
+                    <span class="about__item-icon"><?= get_template_part('svg/globe') ?></span>
+                    HTML & CSS
+                </p>
+                <ul class="about__item-skills">
+                    <li class="about__item-skill">Semantic</li>
+                    <li class="about__item-skill">Post CSS</li>
+                    <li class="about__item-skill">Animations & Transitions</li>
+                </ul>
+            </li>
+            <li class="about__item">
+                <p class="about__item-meta">Intermediate</p>
+                <p class="about__item-title">
+                    <span class="about__item-icon"><?= get_template_part('svg/javascript') ?></span>
+                    JavaScript
+                </p>
+                <ul class="about__item-skills">
+                    <li class="about__item-skill">ES6</li>
+                    <li class="about__item-skill">Node</li>
+                    <li class="about__item-skill">React</li>
+                    <li class="about__item-skill">Webpack & Gulp</li>
+                </ul>
+            </li>
+            <li class="about__item">
+                <p class="about__item-meta">Intermediate</p>
+                <p class="about__item-title">
+                    <span class="about__item-icon"><?= get_template_part('svg/php') ?></span>
+                    PHP
+                </p>
+                <ul class="about__item-skills">
+                    <li class="about__item-skill">OOP</li>
+                    <li class="about__item-skill">Composer</li>
+                    <li class="about__item-skill">WordPress</li>
+                </ul>
+            </li>
+        </ul>
     </div>
 </div>
