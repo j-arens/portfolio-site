62f5618a25ef7d7065bcc9aab6a6d2028b270072
j-arens <arens.joshua@sbcglobal.net>
2017-07-28 11:27:59 -0400

    sniff out all versions of ie and display warning to upgrade

--- source/templates/layouts/base.php
+++ source/templates/layouts/base.php
@@ -2,16 +2,24 @@
 <html <?php language_attributes(); ?>>
   <?php get_template_part('partials/head'); ?>
   <body <?php body_class(); ?>>
-    <!--[if IE]>
-      <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'spine'); ?>
-      </div>
-    <![endif]-->
+
+    <!-- warn IE users to upgrade to a better browser -->
+    <script>
+        if (!(window.ActiveXObject) && 'ActiveXObject' in window) {
+            document.body.insertAdjacentHTML(
+                'afterbegin', 
+                '<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://outdatedbrowser.com/">upgrade your browser</a> to view this site properly.<p>'
+            );
+        }
+    </script>
+
+    <!--  feature detect mix-blend-mode  -->
     <script>
       if ('mixBlendMode' in document.body.style) {
           document.body.classList.add('supports-mixBlendMode');
       }
     </script>
+
     <?= get_template_part('svg/topography') ?>
     <div class="wrap" role="document">
       <main class="main">
