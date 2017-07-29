53c904a91d98b6ddcefa2856091ef57c70eed4da
Josh <arens.joshua@sbcglobal.net>
2017-07-29 15:33:08 -0400

    add 404 page

--- source/templates/404.php
+++ source/templates/404.php
@@ -1,4 +1,9 @@
-<div class="not-found">
-  <p>Sorry, we couldn't find the page you're looking for.</p>
-  <?php get_search_form(); ?>
+<div class="card not-found">
+  <p class="not-found__message">
+    404
+  </p>
+  <a href="#" onclick="history.go(-1)" class="not-found__link btn btn-primary">
+    <?= get_template_part('svg/left-arrow') ?>
+    <span class="not-found__link-text">Go Back</span>
+  </a>
 </div>
